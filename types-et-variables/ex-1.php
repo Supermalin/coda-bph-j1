993e55eb62d31218a98c3e46de4d165ba677cc74
cizeautom <cizeautom404@gmail.com>
2025-11-20 10:51:18 +0100

    exercice 2 fait

---
+++ types-et-variables/ex-1.php
@@ -0,0 +1,9 @@
+<?php
+	$nb=42;
+	// echo $nb;
+	$str=" La vie l'univers et tout le reste";
+	// echo $str;
+	$nbtostr=(string) $nb;
+	$nbstr= $nbtostr.$str;
+	echo $nbstr;
+?>
---
+++ types-et-variables/ex-2.php
@@ -0,0 +1,9 @@
+<?php
+	$nb_tab = [23, 45.5, 18];
+    print_r($nb_tab);
+
+    echo "<br>";
+
+    $str_tab = ["Cat", "Dog", "Rabbit"];
+    print_r($str_tab);
+?>
